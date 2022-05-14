e730934a9d023ebabac4b19c40a82355215fd2d0
LUIS ALEJANDRO GONZALEZ GONZALEZ <luisalejandrogonzalez@ucundinamarca.edu.co>
2022-05-14 14:04:14 -0500

    .

---
+++ Controller/Factura.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class View_Factura : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void B_Generar_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Reporte inf = llenarReporte();
+            CRS_Factura.ReportDocument.SetDataSource(inf);
+            CRV_Factura.ReportSource = CRS_Factura;
+
+            CRV_Factura.Visible = true;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
+
